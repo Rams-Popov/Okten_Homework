d59e17bddb189cdc3cb10de4e82005e6529e887a
Rams-Popov <100295237+Rams-Popov@users.noreply.github.com>
2022-04-07 22:15:10 +0300

    huh

--- Homework_2/many_bloks/js.js
+++
@@ -1,6 +0,0 @@
-function Click(){
-    var oShell = new ActiveXObject("Shell.Application");
-    var commandtoRun = "C:\\Program Files\\tvnc\\tvnviewer.exe";
-    // oShell.ShellExecute(commandtoRun,"","","open","1");
-    oShell.open(commandtoRun);
-}
