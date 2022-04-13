298439bbb79ed1491fdcf9f45dc9bd57ba9c06e1
Rams_Popov <sanchoss298@gmail.com>
2022-04-13 23:26:57 +0300

    homework JS

---
+++ Homework_5/script/main.js
@@ -0,0 +1,40 @@
+let str1 = "hello";
+let str2 = "owu";
+let str3 = "com";
+let str4 = "ua";
+
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+const number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+
+const boolean1 = true;
+const boolean2 = false;
+
+alert(str1);
+alert(str2);
+alert(str3);
+alert(str4);
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number6);
+console.log(number7);
+
+document.write(boolean1);
+document.write(boolean2);
+
+
+let firstObject = {
+    name: "Harry Potter",
+    pages: 800,
+    genre: "fantastic"
+}
+
+
