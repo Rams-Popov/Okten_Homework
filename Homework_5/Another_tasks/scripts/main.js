0acc26193c931389df212f77a03da7aedda25b99
Rams-Popov <100295237+Rams-Popov@users.noreply.github.com>
2022-04-14 19:39:59 +0300

    JS another tasks

---
+++ Homework_5/Another_tasks/scripts/main.js
@@ -0,0 +1,27 @@
+let height = prompt("Введіть висоту прямокутника");
+let width = prompt("Ведіть ширину прямлкутника");
+let s = height * width;
+alert(`Площа прямокутника дорівнює ${s}`);
+
+let heightC = 10;
+let dC = 4;
+const PI = 3.14;
+v = PI * (dC / 2) ** 2 * heightC;
+console.log(`Об'єм циліндра дорівнює ${v}`);
+
+let n = 3;
+let m = 4;
+let k = Math.pow(n, 2) + Math.pow(m, 2);
+console.log(`Гіпотенуза трикутника дорівнює ${k ** 0.5}`);
+
+let bool1 = 5 < 6;
+let bool2 = 5 > 6;
+let bool3 = 5 == 6;
+let bool4 = 5 === 6;
+let bool5 = 10 == 10;
+let bool6 = 10 === 10;
+let bool7 = 10 > 10;
+let bool8 = 10 < 10;
+let bool9 = 10 != 10;
+let bool10 = 123 === '123';
+let bool11 = 123 == '123';
