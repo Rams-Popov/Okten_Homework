cb9b3c09bd9b2010d8e11e29bc63c8d4b48a952b
Rams-Popov <100295237+Rams-Popov@users.noreply.github.com>
2022-05-26 14:34:17 +0300

    11 task

---
+++ HTML_CSS_JS/Homework_15/main_tasks/scripts/main.js
@@ -0,0 +1,48 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+//
+// const formOne = document.createElement("form");
+// const inputName = document.createElement("input");
+// const inputAge = document.createElement("input");
+// const send1 = document.createElement("button");
+//
+// formOne.name = "form1";
+// inputName.name = "name";
+//
+// inputAge.type = "number";
+// inputAge.name = "age";
+//
+// send1.innerText = "Send";
+//
+// formOne.append(inputName, inputAge, send1);
+// document.body.appendChild(formOne);
+//
+// send1.addEventListener("click",()=>{
+//     const objectUser = {name: inputName.value, age: inputAge.value};
+//     localStorage.setItem("user", JSON.stringify(objectUser));
+// })
+
+
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+const formTwo = document.createElement("form");
+const inputModel = document.createElement("input");
+const inputType = document.createElement("input");
+const inputVolume = document.createElement("input");
+const send2 = document.createElement("button");
+
+formTwo.name = "form2";
+inputModel.name = "model";
+inputType.name = "type";
+inputVolume.name = "volume";
+
+send2.innerText = "Send2";
+
+formTwo.append(inputModel, inputType, inputVolume, send2);
+document.body.appendChild(formTwo);
+
+send2.addEventListener("click",()=>{
+    const objectUser = {model: inputModel.value, type: inputType.value, volume: inputVolume.value};
+    localStorage.setItem("user", JSON.stringify(objectUser));
+})
